9c125a9fcfa7f413c86136d38938a5e9e2c130e4
Tim Jones <timhojones@googlemail.com>
2019-02-07 12:10:38 +0000

    Created inital setup script
    This will *probably* work

---
+++ setup.sh
@@ -0,0 +1,19 @@
+# Name the dock volumne with the CA
+OVPN_DATA="ovpn-data-main"
+
+# Create the docker volumne
+docker volume create --name $OVPN_DATA
+
+# Add the files needed to run the ovpn servace into the docker voumne
+######### Need to check what the VPN.SERVERNAME.COM sould be
+docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_genconfig -u udp://VPN.SERVERNAME.COM
+docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki
+
+# Start the OVPN service
+docker run -v $OVPN_DATA:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
+
+# Create the clinat cert 
+docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn easyrsa build-client-full CLIENTNAME nopass
+
+# Cet the clinat cert in a usable form, with the certificates
+docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_getclient CLIENTNAME > CLIENTNAME.ovpn
