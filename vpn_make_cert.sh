71be9449a0c05703c20bc29ebcbc38043595171d
Tim113 <timhojones@gmail.com>
2019-09-15 12:46:49 +0000

    Fix the vpn setup?

--- vpn_make_cert.sh
+++ vpn_make_cert.sh
@@ -1,14 +1,23 @@
-OVPN_DATA="ovpn-data-main"
+# Name the dock volumne with the CA
+TOP_LEVEL_NAME="basis"
+OVPN_DATA="ovpn-data-basis"
+OVPN_NETWORK="ovpn-basis"
+OVPN_PORT=1195
+OVPN_SERVER_NAME="vpn.thj113.com"I
+OVPN_DNS=167.99.211.211
+CERT_NAME='tim_oneplus6'
 
 # Create the clinat cert 
 docker run \
-	-v $OVPN_DATA:/etc/openvpn \
-	-e EASYRSA_KEY_SIZE=4096 \
-	--log-driver=none --rm \
-	-it kylemanna/openvpn easyrsa build-client-full TEST nopass
+    -v $OVPN_DATA:/etc/openvpn \
+    -e EASYRSA_KEY_SIZE=4096 \
+    --network=$OVPN_NETWORK \
+    --log-driver=none --rm \
+    -it kylemanna/openvpn easyrsa build-client-full $CERT_NAME nopass
 
 # Cet the clinat cert in a usable form, with the certificates
 docker run \
-	-v $OVPN_DATA:/etc/openvpn \
-       --log-driver=none \
-       --rm kylemanna/openvpn ovpn_getclient TEST > TEST.ovpn.tst
+    -v $OVPN_DATA:/etc/openvpn \
+    --network=$OVPN_NETWORK \
+    --log-driver=none \
+    --rm kylemanna/openvpn ovpn_getclient $CERT_NAME > $CERT_NAME.ovpn
--- vpn_setup.sh
+++ vpn_setup.sh
@@ -1,8 +1,14 @@
 # Name the dock volumne with the CA
-OVPN_DATA="ovpn-data-pi-hole"
+TOP_LEVEL_NAME="main"
+OVPN_DATA="ovpn-data-main"
+OVPN_NETWORK="ovpn-net-main"
+OVPN_CONTAINER_NAME='ovnp-main'
 OVPN_PORT=1195
 OVPN_SERVER_NAME="vpn.thj113.com"I
-OVPN_DNS=167.99.211.211
+OVPN_DNS=1.1.1.1
+
+# Create the network that will be used to 
+sudo docker network create $OVPN_NETWORK
 
 # Create the docker volumne
 docker volume create --name $OVPN_DATA
@@ -14,6 +20,7 @@ docker run \
     -e OVPN_DNS='1' \
     -v $OVPN_DATA:/etc/openvpn \
     --log-driver=none \
+    --network=$OVPN_NETWORK \
     --rm kylemanna/openvpn ovpn_genconfig \
     -C 'AES-256-CBC' \
     -a 'SHA384' \
@@ -22,25 +29,30 @@ docker run \
 docker run \
     -e EASYRSA_KEY_SIZE=4096 \
     -v $OVPN_DATA:/etc/openvpn \
+    --network=$OVPN_NETWORK \
     --log-driver=none --rm \
     -it kylemanna/openvpn ovpn_initpki
 
 # Start the OVPN service
 docker run \
-	-v $OVPN_DATA:/etc/openvpn -d -p \
-	$OVPN_PORT:1194/udp \
-	--cap-add=NET_ADMIN kylemanna/openvpn
-	--name=$OVPN_DATA
+    -v $OVPN_DATA:/etc/openvpn -d \
+    --publish $OVPN_PORT:1194/udp \
+    --network=$OVPN_NETWORK \
+    --log-driver=none \
+    --cap-add=NET_ADMIN kylemanna/openvpn \
+    --name=$OVPN_CONTAINER_NAME
 
 ## Create the clinat cert 
 #docker run \
-#	-v $OVPN_DATA:/etc/openvpn \
-#	-e EASYRSA_KEY_SIZE=4096 \
-#	--log-driver=none --rm \
-#	-it kylemanna/openvpn easyrsa build-client-full CLIENTNAME nopass
+#    -v $OVPN_DATA:/etc/openvpn \
+#    -e EASYRSA_KEY_SIZE=4096 \
+#    --network=$OVPN_NETWORK \
+#    --log-driver=none --rm \
+#    -it kylemanna/openvpn easyrsa build-client-full CLIENTNAME nopass
 
 ## Cet the clinat cert in a usable form, with the certificates
 #docker run \
-#	-v $OVPN_DATA:/etc/openvpn \
-#       --log-driver=none \
-#       --rm kylemanna/openvpn ovpn_getclient CLIENTNAME > CLIENTNAME.ovpn
+#    -v $OVPN_DATA:/etc/openvpn \
+#    --network=$OVPN_NETWORK \
+#    --log-driver=none \
+#    --rm kylemanna/openvpn ovpn_getclient CLIENTNAME > CLIENTNAME.ovpn
