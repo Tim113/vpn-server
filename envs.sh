cdf0f393cd1c7c9a4e601818340be0e887b79fbb
Tim113 <timhojones@gmail.com>
2020-03-01 17:49:24 +0000

    Change the env file so that it works

--- envs.sh
+++ envs.sh
@@ -1,8 +1,8 @@
 # Name the dock volumne with the CA
-TOP_LEVEL_NAME="main"
-OVPN_DATA="ovpn-data-main"
-OVPN_NETWORK="ovpn-net-main"
-OVPN_CONTAINER_NAME='ovnp-main'
-OVPN_PORT=1194
-OVPN_SERVER_NAME="vpn.thj113.com"
-OVPN_DNS=1.1.1.1
+export TOP_LEVEL_NAME="main"
+export OVPN_DATA="ovpn-data-main"
+export OVPN_NETWORK="ovpn-net-main"
+export OVPN_CONTAINER_NAME='ovnp-main'
+export OVPN_PORT=1194
+export OVPN_SERVER_NAME="vpn.thj113.com"
+export OVPN_DNS=1.1.1.1
